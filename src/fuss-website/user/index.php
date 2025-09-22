e79346518811acb6159654c288a36917335b7841
kazaarplaya <164322774+kazaarplaya@users.noreply.github.com>
2025-09-22 22:43:41 +0930

    docs(index.html): Navbar completed
    Hans Pujalte, Bachelor of Computer Science (Honours), PUJA0009

---
+++ src/fuss-website/user/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>User Home Page</title>
+    <link rel="stylesheet" href="/src/fuss-website/css/style.css" />
+  </head>
+  <body>
+    <?php 
+     include("../inc/header.php");
+     ?>
+
+    <!-- Main Content -->\
+
+    <main></main>
+  </body>
+</html>
