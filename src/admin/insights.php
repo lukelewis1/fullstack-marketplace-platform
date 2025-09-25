10df74819b8527f213d9bd79c0be2c75205e0c9f
puja0009 <puja0009@flinders.edu.au>
2025-09-25 03:06:14 +0000

    feature(src): Added admin dashboard websites

---
+++ src/admin/insights.php
@@ -0,0 +1,30 @@
+<?php
+if (session_status() === PHP_SESSION_NONE) {
+    session_start();
+}
+
+if (!isset($_SESSION['username'])) {
+    header("Location: ../index.php");
+    exit;
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>User Home Page</title>
+    <link rel="stylesheet" href="../styles/style.css" />
+  </head>
+  <body>
+    <?php 
+     include('../inc/admin-header.php');
+     ?>
+
+    <!-- Main Content -->
+     <h1> Insights </h1>
+
+    <main></main>
+  </body>
+</html>
