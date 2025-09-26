08c0d1e98f50dea6520a19bac2c6b6d9d4395c50
txngsten <wutt0019@flinder.edu.au>
2025-09-26 20:07:16 +0930

    bugfix(db-user-pop-script): Added fuss credit insert so script initialises accounts correctly.
    Changes made by Oliver Wuttke, Bachelor of Computer Science (Artificial Intelligence), WUTT0019.

--- db/query/msg_user_populate.sql
+++ db/query/msg_user_populate.sql
@@ -1,11 +1,11 @@
 USE FUSS_DB;
 
-INSERT INTO Users (user_name, email, hashed_password, f_name, l_name, bio, dob, is_admin, acc_status, role)
+INSERT INTO Users (user_name, email, hashed_password, f_name, l_name, bio, dob, is_admin, acc_status, role, fuss_credit)
 VALUES
-    ('Oliver','oliver@example.com','8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918','Oliver','Wuttke','Hi, I am Oliver, testing FUSS.','2000-01-01',TRUE,1,'Admin'),
-    ('Hans','hans@example.com','8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918','Hans','Zimmer','Music lover and producer.','1995-03-15',FALSE,1,'User'),
-    ('Luke','luke@example.com','8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918','Luke','Skywalker','Aspiring Jedi trader.','1998-05-04',FALSE,1,'User'),
-    ('Seth','seth@example.com','8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918','Seth','Rogan','Comedian exploring the platform.','1992-08-20',FALSE,1,'User');
+    ('Oliver','oliver@example.com','8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918','Oliver','Wuttke','Hi, I am Oliver, testing FUSS.','2000-01-01',TRUE,1,'Admin', 5),
+    ('Hans','hans@example.com','8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918','Hans','Zimmer','Music lover and producer.','1995-03-15',FALSE,1,'User', 5),
+    ('Luke','luke@example.com','8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918','Luke','Skywalker','Aspiring Jedi trader.','1998-05-04',FALSE,1,'User', 5),
+    ('Seth','seth@example.com','8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918','Seth','Rogan','Comedian exploring the platform.','1992-08-20',FALSE,1,'User', 5);
 
 INSERT INTO Friendships (user_id, friend_id, status)
 VALUES
