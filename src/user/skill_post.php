f0ee456d23f57b7a6419b44f944b5d748295b023
txngsten <wutt0019@flinder.edu.au>
2025-09-27 12:47:40 +0930

    feat(skill-posting): Added initial html for the skill_post.php page. Still needs styling, scripting and DB support along with validation on if the skill was posted or not.
    Changes made by Oliver Wuttke, Bachelor of Computer Science (Artificial Intelligence), WUTT0019.

--- src/user/skill_post.php
+++ src/user/skill_post.php
@@ -12,10 +12,11 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>User Home Page</title>
-    <link rel="stylesheet" href="../styles/style.css" />
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>User Home Page</title>
+      <link rel="stylesheet" href="../styles/style.css" />
+      <link rel="stylesheet" href="skill_post_handler/skill_post_styles.css" />
   </head>
   <body>
     <?php
@@ -25,7 +26,63 @@
      ?>
 
     <main class="content">
-      <h1>Skill Post</h1>
+      <div id="post-page">
+          <form id="post-form">
+
+              <div class="posting-field">
+                  <label for="title">Service/Skill Name:</label>
+                  <input type="text" id="title" name="skill-name" required>
+              </div>
+
+              <div class="posting-field">
+                  <label for="topic">Relevant Topic/Degree:</label>
+                  <input type="text" id="topic" name="topic-name" required>
+              </div>
+
+              <div class="posting-field">
+                  <label for="category">Select the Most Relevant Option:</label>
+                  <select id="category" name="category-name" required>
+                      <option value="">-- Please choose an option --</option>
+                      <option value="tutoring">Tutoring</option>
+                      <option value="life_skill">Life Skill</option>
+                      <option value="tech_support">Tech Support</option>
+                      <option value="technical">Technical</option>
+                      <option value="practical">Practical</option>
+                  </select>
+              </div>
+
+              <div class="posting-field">
+                  <label for="description">Description of Skill/Service:</label>
+                  <textarea id="description" rows="15" cols="45" placeholder="Describe your skill or service in detail..."></textarea>
+              </div>
+
+              <div class="posting-field">
+                  <label for="price">Enter the Amount of FUSS Credits for This Service:</label>
+                  <input type="number" id="price" name="price-name" required>
+              </div>
+
+              <div class="posting-field">
+                  <fieldset>
+                      <legend>Is the Price Negotiable?</legend>
+
+                      <label>
+                          <input type="radio" name="negotiable" value="yes" required>
+                          Yes
+                      </label>
+
+                      <label>
+                          <input type="radio" name="negotiable" value="no">
+                          No
+                      </label>
+                  </fieldset>
+              </div>
+
+              <div class="submit-btn">
+                  <button class="btn" type="submit" name="full-reg">Post</button>
+              </div>
+
+          </form>
+      </div>
     </main>
 
     <script src="../scripts/global_scripts.js"></script>
