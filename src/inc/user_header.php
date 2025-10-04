460b6f6c6ee348397fae3d78e421a9afa0103be0
txngsten <wutt0019@flinder.edu.au>
2025-10-04 15:16:04 +0930

    feat(skill-management): Added skill bookings to nav bar and skill_bookings.php will serve as a content area where users can accept, cancel and deny skill requests alongside managing their own skill requests/purchases
    Changes made by Oliver Wuttke, Bachelor of Computer Science (Artificial Intelligence), WUTT0019.

--- src/inc/user_header.php
+++ src/inc/user_header.php
@@ -71,9 +71,10 @@
           <div class="dropdown">
             <li><a  class="<?= ($currentPage == 'skill_manage.php') ? 'active' : '' ?>">Skill Share</a></li>
             <div class="dropdown-content">
-              <a href="/user/skill_post.php">Post Service</a>
-              <a href="/user/skill_manage.php"> Service Management </a>
-              <a href="/user/skill_search.php"> Skill Search </a>
+                <a href="/user/skill_post.php">Post Service</a>
+                <a href="/user/skill_manage.php"> Service Management </a>
+                <a href="/user/skill_search.php"> Skill Search </a>
+                <a href="/user/skill_bookings.php">Skill Bookings</a>
             </div>
           </div>
 
