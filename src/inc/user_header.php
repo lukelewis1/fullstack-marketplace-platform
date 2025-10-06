255734017976e16f78fa52d94d848434f94cde31
puja0009 <puja0009@flinders.edu.au>
2025-10-06 23:03:08 +0000

    feat(src): Moved logout button and made avatar pfp a dropdown menu

--- src/inc/user_header.php
+++ src/inc/user_header.php
@@ -49,10 +49,14 @@
             <span class="user-role"> <?php echo htmlspecialchars(get_role($_SESSION['username'] ?? '')); ?> </span>
           </div>
           <div class="avatar">
-            <a href="/user/profile.php">
-
-              <img src="<?php echo htmlspecialchars(get_profile_image($_SESSION['username'] ?? '')) ?>" alt="Profile Picture" />
-            </a> 
+            <div class="dropdown avatar-dropdown">
+              <img src="<?php echo htmlspecialchars(get_profile_image($_SESSION['username'] ?? '')); ?>" alt="Profile Picture" />
+              <div class="dropdown-content avatar-dropdown-content">
+                  <a href="/user/profile.php">View Profile</a>
+                  <a href="/inc/logout.php" id="logout-link">Logout</a>
+              
+              </div>
+            </div>
             
           </div>
         </div>
@@ -86,15 +90,6 @@
                 </div>
             </div>
 
-            <li><a href="/inc/logout.php" class="<?= ($currentPage == 'logout.php') ? 'active' : '' ?>" id="logout-link">Logout</a></li>
-            <div id="logout-modal" style="display:none; position:fixed; top:0;left:0;width:100%;height:100%;background:rgba(0,0,0,0.5);">
-                <div style="background:#fff;padding:20px;margin:100px auto;width:300px;text-align:center">
-                    <p>Are you sure you want to log out?</p>
-                    <p>You WILL have to log back in to access FUSS.</p>
-                    <button id="logout-yes">Yes</button>
-                    <button id="logout-no">No</button>
-                </div>
-            </div>
 
           <li class="credits"
               class="<?= ($currentPage == 'home.php') ? 'active' : '' ?>">
@@ -102,6 +97,16 @@
                 Credits: <span class="credit-circle"><?php echo htmlspecialchars($credit_balance); ?></span>
               </a>
           </li>
+
+          <div id="logout-modal" style="display:none; position:fixed; top:0;left:0;width:100%;height:100%;background:rgba(0,0,0,0.5);">
+                <div style="background:#fff;padding:20px;margin:100px auto;width:300px;text-align:center" id="logout">
+                    <p>Are you sure you want to log out?</p>
+                    <p>You WILL have to log back in to access FUSS.</p>
+                    <button id="logout-yes">Yes</button>
+                    <button id="logout-no">No</button>
+                </div>
+            </div>
+
         </ul>
       </nav>
     <script src="/scripts/logout_script.js"></script>
