86621d8dbd00cf126c57e1fb01aaab8b4417b8f4
wutt0019 <wutt0019@flinders.edu.au>
2025-10-10 12:25:01 +1030

    feat(calendar): Added the style sheet and js files for the calendar.php. Still needs implementation but the needed data is stored in result array ready to use and dynamically populate the page.
    Oliver Wuttke, WUTT0019, Computer Science (Artificial Intelligence).

--- src/user/calendar.php
+++ src/user/calendar.php
@@ -1,13 +1,28 @@
 <?php
 
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
+    if (session_status() === PHP_SESSION_NONE) {
+        session_start();
+    }
 
-$currentPage = basename($_SERVER['PHP_SELF']);
+    $currentPage = basename($_SERVER['PHP_SELF']);
 
-require_once __DIR__ . '/../inc/dbconn.inc.php';
-require_once __DIR__ . '/../inc/functions.php';
+    require_once __DIR__ . '/../inc/dbconn.inc.php';
+    require_once __DIR__ . '/../inc/functions.php';
+
+    $uid = get_uid($_SESSION['username']);
+
+    $sql = "SELECT * FROM Bookings WHERE booker_id = ? OR service_provider_id = ?;";
+    $statement = $conn->prepare($sql);
+    $statement->bind_param('ii', $uid, $uid);
+    $statement->execute();
+    $result = $statement->get_result();
+
+    $results = [];
+
+    while ($row = $result->fetch_assoc()) {
+        $result[] = $row;
+    }
+    $statement->close();
 
 ?>
 
@@ -18,12 +33,13 @@ require_once __DIR__ . '/../inc/functions.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>FUSS</title>
     <link rel="stylesheet" href="../styles/style.css" />
+    <link rel="stylesheet" href="calendar_handler/calendar_style.css" />
 </head>
 <body>
 <?php
 include_header($_SESSION['username'] ?? null);
 ?>
 
-<h1>Calendar</h1>
+<h1>Bookings Calendar</h1>
 </body>
 </html>
