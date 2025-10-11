99079eec8cf591c25f5fbe38acc466fd59dd56dd
txngsten <wutt0019@flinder.edu.au>
2025-10-11 13:14:25 +1030

    feat(notification-system): notifications.php now displays all types of notifications with links to the relevant parts of the site where those notifications can be managed.
    Changes made by Oliver Wuttke, Bachelor of Computer Science (Artificial Intelligence), WUTT0019.

--- src/notifications/notifications.php
+++ src/notifications/notifications.php
@@ -11,7 +11,7 @@
 
     $uid = get_uid($_SESSION['username']);
 
-    $sql = "SELECT type, time FROM Notifications WHERE user_id = ? ORDER BY time DESC;";
+    $sql = "SELECT type, time, seen FROM Notifications WHERE user_id = ? ORDER BY seen ASC, time DESC;";
     $stmt = $conn->prepare($sql);
     $stmt->bind_param('i', $uid);
     $stmt->execute();
@@ -23,6 +23,32 @@
         $notifications[] = $row;
     }
     $stmt->close();
+
+    foreach ($notifications as &$item) {
+        switch ($item['type']) {
+            case 'service_request':
+                $item['msg'] = "A user has requested a service you provide.";
+                $item['loc'] = '/user/my_skills.php';
+                break;
+            case 'accepted_request':
+                $item['msg'] = "A service provider has accepted one of your requests.";
+                $item['loc'] = '/user/skill_bookings.php';
+                break;
+            case 'completed_service_confirm':
+                $item['msg'] = "A service provider has confirmed service completion, if this is true confirm on your end as well.";
+                $item['loc'] = '/user/skill_bookings.php';
+                break;
+            case 'completed_service':
+                $item['msg'] = "A service has been completed.";
+                $item['loc'] = '#';
+                break;
+            case 'review':
+                $item['msg'] = "You've received a new review.";
+                $item['loc'] = '#';
+                break;
+        }
+    }
+    unset($item);
 
 ?>
 
@@ -44,7 +70,19 @@
 
 <div class="notifications">
     <ul class="my-notifications">
-
+        <?php
+            if (count($notifications) === 0) {
+                echo '<h3 class="skill-title">You Have No Notifications</h3>';
+            }
+            else foreach ($notifications as $item):
+        ?>
+        <div>
+            <li class="notification-res">
+                <h5><a href="<?= htmlspecialchars($item['loc']) ?>"><?= htmlspecialchars($item['msg']) ?></a></h5>
+                <p><?= htmlspecialchars($item['time']) ?></p>
+            </li>
+        </div>
+        <?php endforeach; ?>
     </ul>
 </div>
 
