9375f0ade79d6971ecbbb453db14bd1a4cd72272
txngsten <wutt0019@flinder.edu.au>
2025-10-11 11:47:58 +1030

    refactor(db): Added the notification table so we can track notifications for users.
    Changes made by Oliver Wuttke, Bachelor of Computer Science (Artificial Intelligence), WUTT0019.

--- db/init/schema.sql
+++ db/init/schema.sql
@@ -128,5 +128,6 @@ CREATE TABLE Notifications (
     notification_id INT AUTO_INCREMENT PRIMARY KEY,
     user_id INT NOT NULL,
     type ENUM('service_request', 'accepted_request', 'canceled_request', 'completed_service_confirm', 'completed_service', 'review'),
+    seen BOOL DEFAULT FALSE,
     FOREIGN KEY (user_id) REFERENCES Users(id)
 );
