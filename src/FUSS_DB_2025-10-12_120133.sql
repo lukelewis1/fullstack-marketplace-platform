1ff1ca4356f4fa6f948c3e7eeda1fffcaf948fd8
Oliver Wuttke <wutt0019@flinders.edu.au>
2025-10-12 12:02:41 +0000

    Final db export

---
+++ src/FUSS_DB_2025-10-12_120133.sql
@@ -0,0 +1,329 @@
+/*M!999999\- enable the sandbox mode */ 
+-- MariaDB dump 10.19  Distrib 10.11.14-MariaDB, for debian-linux-gnu (x86_64)
+--
+-- Host: db    Database: FUSS_DB
+-- ------------------------------------------------------
+-- Server version	8.0.43
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `Availability`
+--
+
+DROP TABLE IF EXISTS `Availability`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Availability` (
+  `availability_id` int NOT NULL AUTO_INCREMENT,
+  `service_id` int NOT NULL,
+  `day` enum('monday','tuesday','wednesday','thursday','friday','saturday','sunday') NOT NULL,
+  `start` time NOT NULL,
+  `end` time NOT NULL,
+  PRIMARY KEY (`availability_id`),
+  KEY `service_id` (`service_id`),
+  CONSTRAINT `Availability_ibfk_1` FOREIGN KEY (`service_id`) REFERENCES `Listings` (`listing_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Availability`
+--
+
+/*!40000 ALTER TABLE `Availability` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Availability` ENABLE KEYS */;
+
+--
+-- Table structure for table `Bookings`
+--
+
+DROP TABLE IF EXISTS `Bookings`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Bookings` (
+  `booking_id` int NOT NULL AUTO_INCREMENT,
+  `booker_id` int NOT NULL,
+  `service_provider_id` int NOT NULL,
+  `start` datetime NOT NULL,
+  `end` datetime NOT NULL,
+  `service_id` int NOT NULL,
+  `status` enum('confirmed','pending') DEFAULT 'pending',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  `provider_confirm` tinyint(1) NOT NULL DEFAULT '0',
+  `booker_confirm` tinyint(1) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`booking_id`),
+  KEY `booker_id` (`booker_id`),
+  KEY `service_provider_id` (`service_provider_id`),
+  KEY `service_id` (`service_id`),
+  CONSTRAINT `Bookings_ibfk_1` FOREIGN KEY (`booker_id`) REFERENCES `Users` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `Bookings_ibfk_2` FOREIGN KEY (`service_provider_id`) REFERENCES `Users` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `Bookings_ibfk_3` FOREIGN KEY (`service_id`) REFERENCES `Listings` (`listing_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Bookings`
+--
+
+/*!40000 ALTER TABLE `Bookings` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Bookings` ENABLE KEYS */;
+
+--
+-- Table structure for table `ChatMessages`
+--
+
+DROP TABLE IF EXISTS `ChatMessages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ChatMessages` (
+  `msg_id` int NOT NULL AUTO_INCREMENT,
+  `conversation_id` int NOT NULL,
+  `sender_id` int NOT NULL,
+  `message_text` text NOT NULL,
+  `sent_at` datetime NOT NULL,
+  PRIMARY KEY (`msg_id`),
+  KEY `conversation_id` (`conversation_id`),
+  KEY `sender_id` (`sender_id`),
+  CONSTRAINT `ChatMessages_ibfk_1` FOREIGN KEY (`conversation_id`) REFERENCES `Messages` (`conversation_id`),
+  CONSTRAINT `ChatMessages_ibfk_2` FOREIGN KEY (`sender_id`) REFERENCES `Users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ChatMessages`
+--
+
+/*!40000 ALTER TABLE `ChatMessages` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ChatMessages` ENABLE KEYS */;
+
+--
+-- Table structure for table `Friendships`
+--
+
+DROP TABLE IF EXISTS `Friendships`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Friendships` (
+  `friendship_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `friend_id` int NOT NULL,
+  `requester_id` int NOT NULL,
+  `status` enum('pending','accepted','blocked') DEFAULT 'pending',
+  PRIMARY KEY (`friendship_id`),
+  UNIQUE KEY `unique_friendship` (`user_id`,`friend_id`),
+  KEY `friend_id` (`friend_id`),
+  KEY `requester_id` (`requester_id`),
+  CONSTRAINT `Friendships_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `Users` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `Friendships_ibfk_2` FOREIGN KEY (`friend_id`) REFERENCES `Users` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `Friendships_ibfk_3` FOREIGN KEY (`requester_id`) REFERENCES `Users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Friendships`
+--
+
+/*!40000 ALTER TABLE `Friendships` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Friendships` ENABLE KEYS */;
+
+--
+-- Table structure for table `Listings`
+--
+
+DROP TABLE IF EXISTS `Listings`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Listings` (
+  `listing_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `price` float NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `topic` varchar(60) NOT NULL,
+  `description` varchar(250) DEFAULT NULL,
+  `successful_exchanges` int NOT NULL DEFAULT '0',
+  `is_negotiable` tinyint(1) NOT NULL,
+  `likes` int DEFAULT '0',
+  `dislikes` int DEFAULT '0',
+  `type` varchar(50) NOT NULL,
+  PRIMARY KEY (`listing_id`),
+  UNIQUE KEY `service_owner` (`user_id`,`title`),
+  CONSTRAINT `Listings_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `Users` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `Listings_chk_1` CHECK (((100 > `price`) and (`price` > 0))),
+  CONSTRAINT `Listings_chk_2` CHECK ((`successful_exchanges` >= 0))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Listings`
+--
+
+/*!40000 ALTER TABLE `Listings` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Listings` ENABLE KEYS */;
+
+--
+-- Table structure for table `Messages`
+--
+
+DROP TABLE IF EXISTS `Messages`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Messages` (
+  `conversation_id` int NOT NULL AUTO_INCREMENT,
+  `sender_id` int NOT NULL,
+  `receiver_id` int NOT NULL,
+  `start_date` datetime NOT NULL,
+  `end_date` datetime NOT NULL,
+  `unseen` int DEFAULT NULL,
+  `unseen_2` int DEFAULT NULL,
+  PRIMARY KEY (`conversation_id`),
+  KEY `sender_id` (`sender_id`),
+  KEY `receiver_id` (`receiver_id`),
+  CONSTRAINT `Messages_ibfk_1` FOREIGN KEY (`sender_id`) REFERENCES `Users` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `Messages_ibfk_2` FOREIGN KEY (`receiver_id`) REFERENCES `Users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Messages`
+--
+
+/*!40000 ALTER TABLE `Messages` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Messages` ENABLE KEYS */;
+
+--
+-- Table structure for table `Notifications`
+--
+
+DROP TABLE IF EXISTS `Notifications`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Notifications` (
+  `notification_id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `type` enum('service_request','accepted_request','completed_service_confirm','completed_service','review') DEFAULT NULL,
+  `time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`notification_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `Notifications_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `Users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Notifications`
+--
+
+/*!40000 ALTER TABLE `Notifications` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Notifications` ENABLE KEYS */;
+
+--
+-- Table structure for table `Reviews`
+--
+
+DROP TABLE IF EXISTS `Reviews`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Reviews` (
+  `review_id` int NOT NULL AUTO_INCREMENT,
+  `service_id` int NOT NULL,
+  `type` enum('positive','negative','neutral') NOT NULL DEFAULT 'neutral',
+  `review` varchar(500) NOT NULL,
+  PRIMARY KEY (`review_id`),
+  KEY `service_id` (`service_id`),
+  CONSTRAINT `Reviews_ibfk_1` FOREIGN KEY (`service_id`) REFERENCES `Listings` (`listing_id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Reviews`
+--
+
+/*!40000 ALTER TABLE `Reviews` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Reviews` ENABLE KEYS */;
+
+--
+-- Table structure for table `TransactionHistory`
+--
+
+DROP TABLE IF EXISTS `TransactionHistory`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `TransactionHistory` (
+  `transaction_id` int NOT NULL AUTO_INCREMENT,
+  `service_id` int NOT NULL,
+  `service_title` varchar(50) NOT NULL,
+  `service_topic` varchar(60) NOT NULL,
+  `provider_id` int NOT NULL,
+  `booker_id` int NOT NULL,
+  `price` int NOT NULL,
+  `time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`transaction_id`),
+  KEY `provider_id` (`provider_id`),
+  KEY `booker_id` (`booker_id`),
+  CONSTRAINT `TransactionHistory_ibfk_1` FOREIGN KEY (`provider_id`) REFERENCES `Users` (`id`) ON DELETE CASCADE,
+  CONSTRAINT `TransactionHistory_ibfk_2` FOREIGN KEY (`booker_id`) REFERENCES `Users` (`id`) ON DELETE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `TransactionHistory`
+--
+
+/*!40000 ALTER TABLE `TransactionHistory` DISABLE KEYS */;
+/*!40000 ALTER TABLE `TransactionHistory` ENABLE KEYS */;
+
+--
+-- Table structure for table `Users`
+--
+
+DROP TABLE IF EXISTS `Users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `Users` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_name` varchar(30) NOT NULL,
+  `email` varchar(50) NOT NULL,
+  `hashed_password` varchar(256) NOT NULL,
+  `f_name` varchar(25) NOT NULL,
+  `l_name` varchar(25) NOT NULL,
+  `bio` varchar(500) DEFAULT NULL,
+  `dob` datetime NOT NULL,
+  `is_admin` tinyint(1) NOT NULL,
+  `acc_status` tinyint(1) DEFAULT '1',
+  `role` varchar(25) NOT NULL,
+  `fuss_credit` float NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `user_name` (`user_name`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `Users`
+--
+
+/*!40000 ALTER TABLE `Users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `Users` ENABLE KEYS */;
+
+--
+-- Dumping routines for database 'FUSS_DB'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2025-10-12 12:01:39
